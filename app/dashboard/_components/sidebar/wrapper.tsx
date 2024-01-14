fc7ddb51c53b4f5ac37e74251c375c3d1b852ee3
hbfl3x <hbfl3x@penguin>
2024-01-14 18:25:22 +0200

    fix flickering on the creators sidebar

--- app/dashboard/_components/sidebar/wrapper.tsx
+++ app/dashboard/_components/sidebar/wrapper.tsx
@@ -16,8 +16,8 @@ const Wrapper = ({children}:WrapperProps) => {
   return (
     <aside className={
       cn(
-        'fixed left-0 z-40 h-full w-60 flex flex-col items-center bg-gray-800 border-r border-gray-500/10',
-        collapsed && 'w-[70px]'
+        'fixed left-0 z-40 h-full w-[70px] lg:w-60 flex flex-col items-center bg-gray-800 border-r border-gray-500/10',
+        collapsed && 'lg:w-[70px]'
         )}>
       {children}
     </aside>
